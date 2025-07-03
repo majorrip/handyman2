ccb27fe9016aa1d6aefedeac225e0899360e8fe7
Atif_Karim <61910778+majorrip@users.noreply.github.com>
2025-07-03 22:31:56 +0600

    Update about-us.php

--- about-us.php
+++ about-us.php
@@ -12,7 +12,7 @@ $rsselectwebpage = mysqli_fetch_array($qsqlselectwebpage);
                     <div class="row">
                         <div class="col-lg-12">
                             <ul class="breadcrumb-list">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item"><a href="#">Homer</a></li>
                                 <li class="breadcrumb-item active">About us</li>
                             </ul>
                         </div>
